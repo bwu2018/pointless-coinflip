a589f9777969f3df1682c0a2a328eefb68480411
Brandon Wu <brandonmwu888@gmail.com>
2022-02-23 12:59:06 -0500

    Added timestamps.

--- sim.py
+++ sim.py
@@ -2,6 +2,8 @@ import random
 import atexit
 import sys
 import argparse
+import time
+from tracemalloc import start
 
 parser = argparse.ArgumentParser(description='Simulates coin flips')
 parser.add_argument('--quiet','-q', action='store_true', help='Run in quiet mode. Do not print out new max streaks')
@@ -16,15 +18,16 @@ max_count = 0
 max_count_value = 'none'
 total_coins = 1
 streak_coins = 1
+start_time = time.time()
 
 @atexit.register
 def print_streak():
     global curr_count, prev_coin, max_count, max_count_value
     if max_count > 0:
-        print(f'{max_count} {max_count_value}')
+        print(f'{max_count} {max_count_value} [{round(time.time() - start_time, 2)}]')
 
 def main():
-    global curr_count, prev_coin, max_count, max_count_value, total_coins, streak_coins
+    global curr_count, prev_coin, max_count, max_count_value, total_coins, streak_coins, start_time
     flags = parser.parse_args()
     while True:
         curr_coin = random.randint(0,1)
@@ -37,7 +40,7 @@ def main():
                 max_count_value = 'heads' if prev_coin else 'tails'
                 max_count = curr_count
                 if not flags.quiet:
-                    print(f'New max streak {max_count} {max_count_value}')
+                    print(f'New max streak {max_count} {max_count_value} [{round(time.time() - start_time, 2)}]')
                 if flags.total:
                     print(f'Total coins flipped {total_coins}')
                 if flags.count:
